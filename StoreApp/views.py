09b702c74f4ad6576cb71ae51ef38aa23aa15462
David Santos <davidsansilva4@gmail.com>
2026-06-27 12:07:04 -0300

    Criando template da home

--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from StoreApp.models import Departamento, Produto
+
 
 # Create your views here.
+def index(request):
+    departamentos = Departamento.objects.all()
+    produtos = Produto.objects.all()
+
+
+    context = {
+        'departamentos': departamentos
+        'produtos': produtos
+        }
+    return render(request, 'index.html', context)
+
+def produtos(request):
+    context = {}
+    return render(request, 'produtos.html', context)
